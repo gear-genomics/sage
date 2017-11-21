f2c5e906a4cf90ed303431f21e3150a303bcecb1
tobiasrausch <rauschtobi@gmail.com>
2017-11-21 16:23:22 +0100

    dependencies

---
+++ src/sage.cpp
@@ -0,0 +1,119 @@
+/*
+============================================================================
+Sage: Sanger Trace Alignment
+============================================================================
+Copyright (C) 2017 Tobias Rausch
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+============================================================================
+Contact: Tobias Rausch (rausch@embl.de)
+============================================================================
+*/
+
+#define BOOST_DISABLE_ASSERTS
+#include <boost/multi_array.hpp>
+#include <boost/program_options/cmdline.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/progress.hpp>
+
+#include <sdsl/suffix_arrays.hpp>
+
+#include <htslib/faidx.h>
+
+#include "abif.h"
+//#include "refslice.h"
+//#include "align.h"
+//#include "gotoh.h"
+
+using namespace sdsl;
+
+struct Config {
+  uint16_t filetype;   //0: *fa.gz, 1: *.fa, 2: *.ab1
+  uint16_t kmer;
+  uint16_t linelimit;
+  uint16_t madc;
+  float pratio;
+  std::string outprefix;
+  boost::filesystem::path ab;
+  boost::filesystem::path genome;
+};
+
+int main(int argc, char** argv) {
+  Config c;
+  
+  // Parameter
+  boost::program_options::options_description generic("Generic options");
+  generic.add_options()
+    ("help,?", "show help message")
+    ("genome,g", boost::program_options::value<boost::filesystem::path>(&c.genome), "(gzipped) fasta or wildtype ab1 file")
+    ("pratio,p", boost::program_options::value<float>(&c.pratio)->default_value(0.33), "peak ratio to call base")
+    ("kmer,k", boost::program_options::value<uint16_t>(&c.kmer)->default_value(15), "kmer size")
+    ;
+
+  boost::program_options::options_description otp("Output options");
+  otp.add_options()
+    ("output,o", boost::program_options::value<std::string>(&c.outprefix)->default_value("align"), "output file prefix")
+    ("linelimit,l", boost::program_options::value<uint16_t>(&c.linelimit)->default_value(60), "alignment line length")
+    ;
+
+  boost::program_options::options_description hidden("Hidden options");
+  hidden.add_options()
+    ("madc,c", boost::program_options::value<uint16_t>(&c.madc)->default_value(5), "MAD cutoff")
+    ("input-file", boost::program_options::value<boost::filesystem::path>(&c.ab), "ab1")
+    ;
+
+  boost::program_options::positional_options_description pos_args;
+  pos_args.add("input-file", -1);
+
+  boost::program_options::options_description cmdline_options;
+  cmdline_options.add(generic).add(otp).add(hidden);
+  boost::program_options::options_description visible_options;
+  visible_options.add(generic).add(otp);
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(cmdline_options).positional(pos_args).run(), vm);
+  boost::program_options::notify(vm);
+
+  // Check command line arguments
+  if ((vm.count("help")) || (!vm.count("input-file"))) {
+    std::cout << "Usage: " << argv[0] << " [OPTIONS] trace.ab1" << std::endl;
+    std::cout << visible_options << "\n";
+    return -1;
+  }
+
+  // Show cmd
+  boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+  std::cout << '[' << boost::posix_time::to_simple_string(now) << "] ";
+  for(int i=0; i<argc; ++i) { std::cout << argv[i] << ' '; }
+  std::cout << std::endl;
+
+  // Load *.ab1 file
+  now = boost::posix_time::second_clock::local_time();
+  std::cout << '[' << boost::posix_time::to_simple_string(now) << "] " << "Load ab1 file" << std::endl;
+  
+  now = boost::posix_time::second_clock::local_time();
+  std::cout << '[' << boost::posix_time::to_simple_string(now) << "] " << "Done." << std::endl;
+
+  return 0;
+}
